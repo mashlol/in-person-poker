e5d2587f3fdc14ce0fc01581e88ad7474eaeeb85
Kevin Bedi <kevin@kevinbedi.com>
2013-07-14 14:39:21 -0400

    Add nice alert & placeholders

---
+++ js/nice-alert.js
@@ -0,0 +1,32 @@
+window.alert = function(message) {
+	var alertDiv = $("<div>").css({
+		width: "100%",
+		background: "#e74c3c",
+		color: "#fff",
+		transform: "translate3d(0,100%,0)",
+		transition: "all 200ms ease-in-out",
+		position: "fixed",
+		bottom: 0, left: 0,
+		padding: "10px",
+		"text-align": "center",
+		"box-sizing": "border-box",
+		"z-index": "9999"
+	}).html(message);
+	$("body").prepend(alertDiv);
+	setTimeout(function(){
+		alertDiv.css({
+			transform: "translate3d(0,0,0)"
+		});
+	}, 0);
+
+	(function(alertDiv) {
+		setTimeout(function() {
+			alertDiv.css({
+				transform: "translate3d(0,100%,0)"
+			});
+			setTimeout(function() {
+				alertDiv.remove();
+			}, 200);
+		}, 1000);
+	})(alertDiv);
+};
